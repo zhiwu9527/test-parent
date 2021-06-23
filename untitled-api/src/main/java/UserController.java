6856aa379157e5c2b9141eaa5f6dc8278f7350c8
zhu_wm <zhiwu9587@gmail.com>
2021-06-23 14:43:28 +0800

    Initial commit

---
+++ untitled-api/src/main/java/UserController.java
@@ -0,0 +1,7 @@
+/**
+ * @author zhu_wm
+ * @date 2021/5/30 8:58
+ */
+public class UserController {
+
+}
---
+++ untitled-core/src/main/java/request/User.java
@@ -0,0 +1,13 @@
+package request;
+
+/**
+ * @author zhu_wm
+ * @date 2021/5/30 8:57
+ */
+public class User {
+
+    private String name;
+
+    private int age;
+
+}
